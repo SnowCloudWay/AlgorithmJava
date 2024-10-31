c151158eb12e7291cf8a18c2727569b692dc2561
name1 <snowcloudway@gmail.com>
2024-11-01 01:21:03 +0900

    commit

--- src/baekjoon/silver/silver4/Main_9012.java
+++ src/baekjoon/silver/silver4/Main_9012.java
@@ -10,7 +10,7 @@ public class Main_9012 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
         int t = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < t; i++) {
